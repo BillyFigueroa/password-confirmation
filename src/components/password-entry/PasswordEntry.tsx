390384558a60a7ed392076352b3e6806a27107f4
Billy Figueroa <BillyJ.Figueroa@gmail.com>
2025-03-20 22:57:09 -0400

    Adding react-hook-form & zod for state management & validation

--- src/components/password-entry/PasswordEntry.tsx
+++ src/components/password-entry/PasswordEntry.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { z } from 'zod'
+import { useForm  } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const PasswordEntry = () => {
-  const handleOnSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Button clicked', e);
+  const pwRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={[}\]|:;"'<,>.]).{6,}$/
+
+  const passwordSchema = z.object({
+    password: z.string()
+      .nonempty('Password is required')
+      .min(6, 'Password should be at least 6 characters long')
+      .regex(pwRegex, 'Password should have correct format'),
+    passwordConfirmation: z.string()
+      .nonempty('Password is required')
+      .min(6, 'Password should be at least 6 characters long')
+      .regex(pwRegex, 'Password should have correct format'),
+  }).refine(data => data.password === data.passwordConfirmation, {
+    message: 'Passwords don\'t match',
+    path: ['passwordConfirmation']
+  })
+
+  type PasswordEntryType = z.infer<typeof passwordSchema>
+
+  const { formState: { errors, isSubmitting }, handleSubmit, register } = useForm<PasswordEntryType>({
+    resolver: zodResolver(passwordSchema)
+  });
+
+  const handleOnSubmit = (formData: PasswordEntryType) => {
+    console.log('Button clicked', formData);
   }
 
   return (
@@ -11,7 +34,7 @@ const PasswordEntry = () => {
       <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create your account password</h2>
 
       <div className="mt-10 w-full">
-        <form className="space-y-6" onSubmit={handleOnSubmit}>
+        <form className="space-y-6" onSubmit={handleSubmit(handleOnSubmit)}>
           <div>
             <label
               htmlFor="password"
@@ -22,13 +45,13 @@ const PasswordEntry = () => {
 
             <div className="mt-2">
               <input
-                type="password"
                 placeholder="i.e. F$Lp.*iCpbA2wqN"
-                name="password"
                 id="password"
+                {...register('password')}
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#246be8] placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#246be8] sm:text-sm/6"
               />
             </div>
+            <div>{errors.password && <div className="text-red-500">{errors.password?.message}</div>}</div>
           </div>
 
           <div>
@@ -43,16 +66,18 @@ const PasswordEntry = () => {
               <input
                 type="password-confirmation"
                 placeholder="i.e. F$Lp.*iCpbA2wqN"
-                name="password-confirmation"
                 id="password-confirmation"
+                {...register('passwordConfirmation')}
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#246be8] placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#246be8] sm:text-sm/6"
               />
             </div>
+            <div>{errors.passwordConfirmation && <div className="text-red-500">{errors.passwordConfirmation?.message}</div>}</div>
           </div>
 
           <button
             type="submit"
             className="flex w-full justify-center rounded-md bg-[#246be8] text-white px-3 py-1.5 text-sm/6 font-semibold shadow-xs hover:bg-[#00539b] focus-visible:outline-2 focus-visible:outline-offset-2 mb-10"
+            disabled={isSubmitting}
           >
             Create password
           </button>
